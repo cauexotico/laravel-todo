a545e6d019af98f1088f9712f1cc69f48ef7b74b
Khauê Poerner <khaue.po@hotmail.com>
2022-11-18 14:57:07 -0300

    refactor: updating breeze (dark mode and profile page)

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\TaskGroupController;
 use Illuminate\Support\Facades\Route;
@@ -23,9 +24,15 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
 Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::resource('task-group', TaskGroupController::class);
     Route::resource('task-group/{task_group}/task', TaskController::class);
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
